3677be00def5e47e842462bbecc46f8a51b7d419
Peter Toth1 <peter_toth1@epam.com>
2017-10-16 17:39:48 +0200

    display hello word

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req,res)=>{res.send('Hello Word!')});
+
 app.listen(3000, () =>{
     console.log('Server started listening on port: 3000');
 });
