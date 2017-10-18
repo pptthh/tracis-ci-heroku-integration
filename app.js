04be332304d0ab3d7bb9db7223dc1aaae763260a
Péter Tóth <peter_toth1@epam.com>
2017-10-18 12:45:01 +0200

    try to fix heroku

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const router = express.Router();
+//const router = express.Router();
 const app = express();
+module.exports = app;
 const log = console.log;
 
 app.get('/', (req,res)=>{res.send('Hello Word!');log('app.get')});
 
 app.post('/author', (req,res) => {res.send({name:'Péter'});log('app.post')});
 
-const server = app.listen(3000, () =>{
+app.listen(3000, () =>{
     console.log('Server started listening on port: 3000');
 });
-module.exports = app;
