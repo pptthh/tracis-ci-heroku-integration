4c6959ff4ca123f0f60258ac6426c7eb46e6304e
Péter Tóth <peter_toth1@epam.com>
2017-10-18 10:18:09 +0200

    try to fix, doing minor changes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
-
+const router = express.Router();
 const app = express();
+const log = console.log;
 
-app.get('/', (req,res)=>{res.send('Hello Word!')});
+app.get('/', (req,res)=>{res.send('Hello Word!');log('app.get')});
 
-app.post('/author', (req,res) => {res.send({name:'Péter'})});
+app.post('/author', (req,res) => {res.send({name:'Péter'});log('app.post')});
 
 const server = app.listen(3000, () =>{
     console.log('Server started listening on port: 3000');
 });
-module.exports = server;
+module.exports = app;
--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,17 @@
+'use strict';
+const assert = require('chai').assert;
 const request = require('supertest');
 const app = require('../app');
 module.exports = app;
+
+describe('homepage', () => {
+    it('welcome the user',(done) => {
+        request(app).get('/')
+            .expect(200)
+            .expect('Content-Type', /html/)
+            .expect(/Hello /, done)
+    })
+});
 
 describe('homepage', () => {
     it('welcome the user',(done) => {
@@ -14,8 +25,12 @@ describe('author', () => {
     it('check author',(done) => {
         request(app).post('/author')
             .expect(200)
+            .expect('Content-Type', /json/)
             .expect(/Péter/, done)
     })
+
+    it('check author',(done) => {
+        request(app).post('/author')
+            .end(done)
+    })
 });
-
-
