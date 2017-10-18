4a231c269e48aaac5457b4958002dffbf24ec1c8
Péter Tóth <peter_toth1@epam.com>
2017-10-18 11:04:25 +0200

    fix testing issue by adding: --exit

--- test/app.test.js
+++ test/app.test.js
@@ -34,3 +34,15 @@ describe('author', () => {
             .end(done)
     })
 });
+
+describe('a suite of tests', function() {
+    this.timeout(500);
+  
+    it('should take less than 500ms', function(done){
+      setTimeout(done, 300);
+    });
+  
+    it('should take less than 500ms as well', function(done){
+      setTimeout(done, 350);
+    });
+});
