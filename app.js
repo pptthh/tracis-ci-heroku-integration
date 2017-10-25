f75549381284e3a92d8bb8acfd3477be1096c2a3
Péter Tóth <peter_toth1@epam.com>
2017-10-25 16:57:43 +0200

    start implementing socket.io

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 //const router = express.Router();
 const app = express();
 module.exports = app;
+
+//  socket io
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 const log = console.log;
 // process.env.PORT lets the port be set by Heroku
 const port = process.env.PORT || 3000;
@@ -10,6 +15,8 @@ app.get('/', (req,res)=>{res.send('Hello Word!');log('app.get')});
 
 app.post('/author', (req,res) => {res.send({name:'Péter'});log('app.post')});
 
-app.listen(port, () =>{
-    console.log(`Server started listening on port: ${port}`);
-});
+app.listen(port, () =>{log(`Server started listening on port: ${port}`);});
+
+app.get('/chat', (req, res)=>{res.sendFile(__dirname + '/index.html')});
+
+io.on('connection', (socket)=>{log('a user connected',socket);});
